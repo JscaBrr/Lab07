fca4a3749adde17b0710471d0a4997b98bfa3738
JscaBrr <barroerojessica@yahoo.com>
2025-04-11 08:55:56 +0200

    soluzione 0

--- model/situazione.py
+++ model/situazione.py
@@ -7,6 +7,18 @@ class Situazione:
     localita: str
     data: datetime.date
     umidita: int
+    #tmedia: int
+    #tmin: int
+    #tmax: int
+    #puntorugiada: int
+    #visibilita: int
+    #ventomedia: int
+    #ventomax: int
+    #raffica: int
+    #pressioneslm: int
+    #pressionemedia: int
+    #pioggia: int
+    #fenomeni: str
 
     def __eq__(self, other):
         return self.localita == other.localita and self.data == other.data
